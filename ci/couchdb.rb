c05f2cd695f9754c4e76d12be6bd8cd942383946
Haissam Kaj <haissam.kaj@datadoghq.com>
2015-02-06 17:39:12 -0500

    Add a timeout and a whitelist to couchdb check, add installation step to CI.

--- ci/couchdb.rb
+++ ci/couchdb.rb
@@ -2,13 +2,37 @@ require './ci/common'
 
 # FIXME: use our own brew of couchdb
 
+def couchdb_version
+  ENV['COUCHDB_VERSION']  || '1.6.1'
+end
+
+def couchdb_rootdir
+  "#{ENV['INTEGRATIONS_DIR']}/couchdb_#{couchdb_version}"
+end
+
 namespace :ci do
   namespace :couchdb do |flavor|
     task :before_install => ['ci:common:before_install']
 
-    task :install => ['ci:common:install']
+    task :install => ['ci:common:install'] do
+      unless Dir.exist? File.expand_path(couchdb_rootdir)
+        sh %(curl -s -L\
+             -o $VOLATILE_DIR/couchdb-#{couchdb_version}.tar.gz\
+             http://mirrors.gigenet.com/apache/couchdb/source/#{couchdb_version}/apache-couchdb-#{couchdb_version}.tar.gz)
+        sh %(mkdir -p #{couchdb_rootdir})
+        sh %(mkdir -p $VOLATILE_DIR/couchdb)
+        sh %(tar zxvf $VOLATILE_DIR/couchdb-#{couchdb_version}.tar.gz\
+             -C $VOLATILE_DIR/couchdb --strip-components=1)
+        sh %(cd $VOLATILE_DIR/couchdb\
+             && ./configure --prefix=#{couchdb_rootdir}\
+             && make\
+             && make install)
+      end
+    end
 
-    task :before_script => ['ci:common:before_script']
+    task :before_script => ['ci:common:before_script'] do
+      sh %(#{couchdb_rootdir}/bin/couchdb -b)
+    end
 
     task :script => ['ci:common:script'] do
       this_provides = [
@@ -17,7 +41,9 @@ namespace :ci do
       Rake::Task['ci:common:run_tests'].invoke(this_provides)
     end
 
-    task :cleanup => ['ci:common:cleanup']
+    task :cleanup => ['ci:common:cleanup'] do
+      sh %(#{couchdb_rootdir}/bin/couchdb -k)
+    end
 
     task :execute do
       exception = nil
